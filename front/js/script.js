c59d2dd3ea3fe1e997bc8188c65cdada484298cc
mouleagaufre <edelin.formations@gmail.com>
2023-06-30 14:18:14 +0200

    Refactoring code

--- front/js/script.js
+++ front/js/script.js
@@ -2,37 +2,30 @@ fetch('http://localhost:3000/api/products')
     .then((response) => response.json())
     .then((data) => {
 
-        const imageUrl = data[0].imageUrl
-        const id = data[0]._id
-        const name = data[0].name
-        const description = data[0].description
-        const prix = data[0].price
+        // boucle forEach permet de parcourir tous les elements de l'API            
+        data.forEach(Kanap => {
 
-        for (let i = 0; i < data.length; i++) {
-
-            const Kanap = data[i]
+            // Déclaration des variables (contenu de l'API)
+            const { imageUrl, _id, name, description, price } = Kanap
 
             // Récuperation de l'element du DOM qui accueillera les fiches
             const items = document.querySelector("#items")
 
-            // Creation de la balise a.href
+            // Création de la balise a.href
             const anchor = document.createElement('a')
-            anchor.href = "./product.html?id=" + id
+            anchor.href = "./product.html?id=" + _id
 
-            // Creation d'une balise dediee au Kanap
+            // Création des balises dediees au Kanap
             const KanapElements = document.createElement("article")
 
-            // Creation de la balise img 
             const imageElement = document.createElement("img")
             imageElement.src = Kanap.imageUrl
             imageElement.text = Kanap.altText
 
-            // Creation de la balise h3
             const nomElement = document.createElement("h3")
             nomElement.add = ("productName")
             nomElement.innerHTML = Kanap.name
 
-            // Creation de la balise p
             const descriptionElement = document.createElement("p")
             descriptionElement.add = ("productDescription")
             descriptionElement.innerHTML = Kanap.description
@@ -48,7 +41,7 @@ fetch('http://localhost:3000/api/products')
             KanapElements.appendChild(imageElement)
             KanapElements.appendChild(nomElement)
             KanapElements.appendChild(descriptionElement)
-        }
+        });
     })
 
     .catch((err) => {
